fa7c053b9245e74ec9fe9314a514b5e41408fad4
David Weschler <davidweschler8@gmail.com>
2025-06-12 23:04:30 +0300

    fix

--- src/main/java/com/app/model/User.java
+++ src/main/java/com/app/model/User.java
@@ -2,17 +2,23 @@ package com.app.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
-import java.util.Set;
 import java.util.HashSet;
-
-
+import java.util.Set;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email"),
+        @UniqueConstraint(columnNames = "googleId")
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
@@ -20,7 +26,7 @@ public class User {
     private Long id;
 
     @NotBlank
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String username;
 
     @Email
@@ -37,6 +43,9 @@ public class User {
     @Column(nullable = false)
     private String role; // "USER" or "ADMIN"
 
+    @Column(nullable = false, unique = true)
+    private String googleId;
+
     private String avatarId;
 
     @CreationTimestamp
@@ -45,7 +54,7 @@ public class User {
     @UpdateTimestamp
     private LocalDateTime updatedAt;
 
+
     @ManyToMany(mappedBy = "members")
     private Set<Chatroom> chatrooms = new HashSet<>();
-
 }
